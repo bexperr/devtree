a851543ba92f95795a7611689678ed651201d644
bexperr <bexperr@gmail.com>
2025-01-13 08:13:04 -0600

    axios and toast

--- frontend/src/views/RegisterView.tsx
+++ frontend/src/views/RegisterView.tsx
@@ -2,7 +2,9 @@ import { Link } from "react-router-dom"
 import { useForm } from "react-hook-form"
 import type { RegisterForm } from "../types";
 import ErrorMessage from "../components/ErrorMessage";
-import axios, {isAxiosError} from "axios"
+import {isAxiosError} from "axios"
+import { toast } from "sonner"
+import {api} from '../config/axios'
 
 export default function RegisterView() {
 
@@ -14,18 +16,18 @@ export default function RegisterView() {
         password_confirmation:''
     }
 
-    const {register, watch, handleSubmit, formState:{errors}} = useForm({defaultValues:initialValues})
+    const {register, watch,reset, handleSubmit, formState:{errors}} = useForm({defaultValues:initialValues})
 
     const password = watch('password')
     
     const handleRegister = async(formData: RegisterForm) => {
         try {
-            const {data} = await axios.post(`${import.meta.env.VITE_API_URL}/auth/register`,formData)
-            console.log(data);
-            
+            const {data} = await api.post(`/auth/register`,formData)
+            toast.success(data)
+            reset()
         } catch (error) {
             if(isAxiosError(error) && error.response){
-                console.log(error.response.data.error);
+                toast.error(error.response.data.error);
                 
             }
         }
